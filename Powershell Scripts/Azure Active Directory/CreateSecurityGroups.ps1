142c03de20be315446b48d3705b1286bc0886587
Rens199991 <renssergier@icloud.com>
2023-02-06 22:29:38 +0100

    1

--- Powershell Scripts/Azure Active Directory/CreateSecurityGroups.ps1
+++ Powershell Scripts/Azure Active Directory/CreateSecurityGroups.ps1
@@ -8,7 +8,7 @@ Disconnect-MgGraph
 
 #Connect and verify that you are in right tenant
 Connect-AzAccount
-Connect-MgGraph -Scopes "Group.readwrite.all"
+Connect-MgGraph -Scopes "User.Read.All","Group.ReadWrite.All"
 
 Get-AzContext | Format-Table Account 
 Get-MgContext | Format-Table Account
