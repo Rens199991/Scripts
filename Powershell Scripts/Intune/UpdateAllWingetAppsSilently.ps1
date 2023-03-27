8fc138217673a17e49fc7a76c6d3186b74a6c042
Rens199991 <renssergier@icloud.com>
2023-03-27 17:23:45 +0200

    change

--- Powershell Scripts/Intune/UpdateAllWingetAppsSilently.ps1
+++ Powershell Scripts/Intune/UpdateAllWingetAppsSilently.ps1
@@ -1,2 +1,5 @@
 #Upgrade siletly all Winget Packages on the PC
 winget upgrade --all --silent --include-unknown
+winget upgrade "discord" --silent
+winget upgrade "Microsoft Teams" --silent
+winget upgrade "Teams Machine-Wide Installer" --silent
