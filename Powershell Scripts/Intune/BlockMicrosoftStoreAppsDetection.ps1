18a2b1c3359429d0bb5c3e87c969bd8de0790906
Rens199991 <renssergier@icloud.com>
2023-07-25 10:14:36 +0200

    Change 2507

---
+++ Powershell Scripts/Intune/BlockMicrosoftStoreAppsDetection.ps1
@@ -0,0 +1,31 @@
+#This blocks the MS Store, but will still allow you to deploy and update apps centrally.
+#https://andrewstaylor.com/2023/07/24/restricting-microsoft-store-via-intune-for-pro-and-enterprise/
+
+$Path = "HKLM:\SOFTWARE\Policies\Microsoft\WindowsStore"
+$Name = "RequirePrivateStoreOnly"
+$Value = 1
+$Registry = Get-ItemProperty -Path $Path -Name $Name -ErrorAction SilentlyContinue | Select-Object -ExpandProperty $Name
+
+
+if ($Registry -eq $Value)
+    {
+    #Create a tag file just so Intune knows this was installed
+    If(-not(Test-Path -Path "$($env:ProgramData)\CXN\Scripts\BlockMicrosoftStoreAppsDRNOT"))
+        {
+        New-Item -itemtype "directory" -path "$($env:ProgramData)\CXN\Scripts\BlockMicrosoftStoreAppsDRNOT"
+        Set-Content -Path "$($env:ProgramData)\CXN\Scripts\BlockMicrosoftStoreAppsDRNOT\BlockMicrosoftStoreAppsDRNOT.ps1.tag" -Value "Installed"
+        }
+    Write-Output "No Remediation Needed"
+    Exit 0
+    }
+Else
+    { 
+    Write-Output "Remediation Needed"
+    #Create a tag file just so Intune knows this was installed
+    If(-not(Test-Path -Path "$($env:ProgramData)\CXN\Scripts\BlockMicrosoftStoreAppsDRNeeded"))
+        {
+        New-Item -itemtype "directory" -path "$($env:ProgramData)\CXN\Scripts\BlockMicrosoftStoreAppsDRNeeded"
+        Set-Content -Path "$($env:ProgramData)\CXN\Scripts\BlockMicrosoftStoreAppsDRNeeded\BlockMicrosoftStoreAppsDRNeeded.ps1.tag" -Value "Installed"
+        }
+    Exit 1
+    }   
