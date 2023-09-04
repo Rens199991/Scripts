6d286bdbda65457ea5a0e70c8914bdbdcb016623
Rens199991 <renssergier@icloud.com>
2023-09-04 13:56:32 +0200

    change

--- Powershell Scripts/Intune/DisableIPV6OnNetworkAdapterRemediation.ps1
+++
@@ -1,9 +0,0 @@
-#Create a tag file just so Intune knows this was installed
-If(-not(Test-Path -Path "$($env:ProgramData)\CXN\Scripts\DisableIPV6OnNetworkAdapterR"))
-    {
-    New-Item -itemtype "directory" -path "$($env:ProgramData)\CXN\Scripts\DisableIPV6OnNetworkAdapterR"
-    Set-Content -Path "$($env:ProgramData)\CXN\Scripts\DisableIPV6OnNetworkAdapterR\DisableIPV6OnNetworkAdapterR.ps1.tag" -Value "Installed"
-    }
-
-
-Disable-NetAdapterBinding -Name “Wi-fi” -ComponentID "ms_tcpip6"
