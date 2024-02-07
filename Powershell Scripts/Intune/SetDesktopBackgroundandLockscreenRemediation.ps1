9e25b660e6b611da6e5998cfbfa84071b957582b
Mr Robot <renssergier@outlook.com>
2024-02-07 12:01:53 +0100

    change 0702

--- Powershell Scripts/Intune/SetDesktopBackgroundandLockscreenRemediation.ps1
+++ Powershell Scripts/Intune/SetDesktopBackgroundandLockscreenRemediation.ps1
@@ -5,31 +5,37 @@ If(-not(Test-Path -Path "$($env:ProgramData)\CXN\Scripts\SetDesktopBackgroundand
     Set-Content -Path "$($env:ProgramData)\CXN\Scripts\SetDesktopBackgroundandLockscreenRemediationR\SetDesktopBackgroundandLockscreenRemediationR.ps1.tag" -Value "Installed"
     }   
 
-#Begin Script to change wallpaper
+#Create a tag file just so Intune knows this was installed
+If (-not(Test-Path -Path "$($env:ProgramData)\CXN\Scripts\SetDesktopBackgroundandLockscreen"))
+    {   
+    New-Item -itemtype "directory" -path "$($env:ProgramData)\CXN\Scripts\SetDesktopBackgroundandLockscreen"
+    Set-Content -Path "$($env:ProgramData)\CXN\Scripts\SetDesktopBackgroundandLockscreen\SetDesktopBackgroundandLockscreen.ps1.tag" -Value "Installed"
+    }
+
+#Begin Script to Change wallpaper
 $RegKeyPath = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\PersonalizationCSP"
 $DesktopPath = "DesktopImagePath"
 $DesktopStatus = "DesktopImageStatus"
 $DesktopUrl = "DesktopImageUrl"
 $StatusValue = "1"
 $url = "https://pim.allinox.be/Allinox/Catalog/General/Allinox_wallpaper.jpg"
-$DesktopImageValue = "$($env:ProgramData)\CXN\Scripts\SetDesktopBackgroundRemediationR\Wallpaper.jpg"
+$DesktopImageValue = "$($env:ProgramData)\CXN\Scripts\SetDesktopBackgroundandLockscreen\Wallpaper.jpg"
 $wc = New-Object System.Net.WebClient
 $wc.DownloadFile($url, $DesktopImageValue)
-        
+
+#Check op RegkeyPath
 if (-not(Test-Path -Path $RegKeyPath))
     {
     Write-Host "Creating registry path $($RegKeyPath)."
     New-Item -Path $RegKeyPath -Force | Out-Null
     }
-        
+
+#We moeten niet werken met Set-ItemProperty als de name-value pairs als bestaan, New-ItemProperty gaat reeds besteende waarden ook overschrijven!
 New-ItemProperty -Path $RegKeyPath -Name $DesktopStatus -Value $Statusvalue -PropertyType DWORD -Force | Out-Null
 New-ItemProperty -Path $RegKeyPath -Name $DesktopPath -Value $DesktopImageValue -PropertyType STRING -Force | Out-Null
 New-ItemProperty -Path $RegKeyPath -Name $DesktopUrl -Value $DesktopImageValue -PropertyType STRING -Force | Out-Null
 
-
-
-<#Begin Script to change Lockscreen
-
+#Begin Script to change Lockscreen
 $RegKeyPath = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\PersonalizationCSP"
 $LockScreenPath = "LockScreenImagePath"
 $LockScreenStatus = "LockScreenImageStatus"
@@ -40,14 +46,14 @@ $LockScreenImageValue = "$($env:ProgramData)\CXN\Scripts\SetDesktopBackgroundand
 $wc = New-Object System.Net.WebClient
 $wc.DownloadFile($url, $LockScreenImageValue)
 
+#Check op RegkeyPath
 if (-not(Test-Path -Path $RegKeyPath))
     {
     Write-Host "Creating registry path $($RegKeyPath)."
     New-Item -Path $RegKeyPath -Force | Out-Null
     }
 
+#We moeten niet werken met Set-ItemProperty als de name-value pairs als bestaan, New-ItemProperty gaat reeds besteende waarden ook overschrijven!
 New-ItemProperty -Path $RegKeyPath -Name $LockScreenStatus -Value $StatusValue -PropertyType DWORD -Force
 New-ItemProperty -Path $RegKeyPath -Name $LockScreenPath -Value $LockScreenImageValue -PropertyType STRING -Force
 New-ItemProperty -Path $RegKeyPath -Name $LockScreenUrl -Value $LockScreenImageValue -PropertyType STRING -Force
-
-#>
