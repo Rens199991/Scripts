4038637e1e541100ca25c43765e86153b076ad9b
Mr Robot <renssergier@outlook.com>
2024-03-06 13:48:00 +0100

    change

---
+++ Powershell Scripts/Intune/BlockMicrosoftPublicStoreRemediation.ps1
@@ -0,0 +1,16 @@
+#Create a tag file just so Intune knows this was installed
+If(-not(Test-Path -Path "$($env:ProgramData)\CXN\Scripts\BlockMicrosoftPublicStoreR"))
+    {
+    New-Item -itemtype "directory" -path "$($env:ProgramData)\CXN\Scripts\BlockMicrosoftPublicStoreR"
+    Set-Content -Path "$($env:ProgramData)\CXN\Scripts\BlockMicrosoftPublicStoreR\BlockMicrosoftPublicStoreR.ps1.tag" -Value "Installed"
+    }
+
+#Begin Script
+$store = "HKLM:\SOFTWARE\Policies\Microsoft\WindowsStore"
+
+If(-not(Test-Path $store))
+    {
+    New-Item $store
+    }
+
+Set-ItemProperty $store RequirePrivateStoreOnly -Value 1 
