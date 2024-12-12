bce00171a68ee1bad40dcc7b10498ecc7059e9d6
Mr Robot <renssergier@outlook.com>
2024-12-12 11:02:17 +0100

    12/12

--- Powershell Scripts/Intune/SyncAllWindowsDevices.ps1
+++ Powershell Scripts/Intune/SyncAllWindowsDevices.ps1
@@ -2,36 +2,23 @@
 #Disconnect-MgGraph
 Disconnect-MgGraph
 
-#Install Needed Modules
-Install-Module Microsoft.Graph.Intune
-Import-Module Microsoft.Graph.Intune 
 
-#Connect to MgGraph & MsGraph
-#Intune Graph Application Registration APi moet de juiste rechten hebben, zie rechten in World of Travel)
-Connect-MgGraph  -Scopes "DeviceManagementManagedDevices.PrivilegedOperations.All"
-Connect-MSGraph -AdminConsent
-Get-MSGraphEnvironment
+#Connect to MgGraph
+Connect-MgGraph -scope DeviceManagementManagedDevices.PrivilegedOperations.All, DeviceManagementManagedDevices.ReadWrite.All,DeviceManagementManagedDevices.Read.All
+
 
 #Get Context
 Get-MgContext
 
-
-$IntuneModule = Get-Module -Name "Microsoft.Graph.Intune" -ListAvailable
-if (!$IntuneModule)
-    {
-    write-host "Microsoft.Graph.Intune Powershell module not installed..." -f Red
-    write-host "Install by running 'Install-Module Microsoft.Graph.Intune' from an elevated PowerShell prompt" -f Yellow
-    write-host "Script can't continue..." -f Red
-    write-host
-    exit
-    }
-
-#Sync all devices running Windows
-$Devices = Get-IntuneManagedDevice -Filter "contains(operatingsystem,'Windows')"
-Foreach ($Device in $Devices)
-    { 
-    Invoke-IntuneManagedDeviceSyncDevice -managedDeviceId $Device.managedDeviceId
-    Write-Host "Sending Sync request to Device with DeviceID $($Device.managedDeviceId)" -ForegroundColor Yellow 
-    }
  
-
+#### Gets All devices
+$Devices = Get-MgDeviceManagementManagedDevice
+ 
+Foreach ($Device in $Devices)
+{
+ 
+Sync-MgDeviceManagementManagedDevice -ManagedDeviceId $Device.Id
+ 
+Write-Host "Sending Sync request to Device with Device name $($Device.DeviceName)" -ForegroundColor Yellow
+  
+}
